43ae7e0babb9e81971a1be8e346134729e856c7d
vaishnavikotha <115101624+vaishnavikotha@users.noreply.github.com>
2024-08-03 11:03:53 +0530

    Add files via upload

---
+++ wellness waves/SCRIPT.js
@@ -0,0 +1,463 @@
+const Depressiondata = [
+  {
+    question:
+      " SECTION 1 : 1. In the past two weeks, how often have you felt down, depressed, or hopeless?",
+      a: "NEVER",
+      b: "SOMETIMES",
+      c: "OFTEN",
+      d: "ALWAYS",
+      s1: "a",
+      s2: "b",
+      s3: "c",
+      s4: "d",
+    type: "deppression",
+  },
+  {
+    question:
+      "SECTION 1 : 2.In the past two weeks, how often have you had little interest or pleasure in doing things ?",
+      a: "NEVER",
+      b: "SOMETIMES",
+      c: "OFTEN",
+      d: " ALWAYS",
+      s1: "a",
+      s2: "b",
+      s3: "c",
+      s4: "d",
+    type: "deppression",
+  },
+  {
+    question:
+      "SECTION 1 : 3.In the past two weeks, how often have you felt tired or had little energy?",
+      a: "NEVER",
+      b: "SOMETIMES",
+      c: "OFTEN",
+      d: " ALWAYS",
+      s1: "a",
+      s2: "b",
+      s3: "c",
+      s4: "d",
+    type: "deppression",
+  },
+  {
+    question: "SECTION 1 : 4.In the past two weeks, how often have you had trouble falling asleep, staying asleep, or sleeping too much? ",
+    a: "NEVER",
+      b: "SOMETIMES",
+      c: "OFTEN",
+      d: " ALWAYS",
+      s1: "a",
+      s2: "b",
+      s3: "c",
+      s4: "d",
+    type: "deppression",
+  },
+  {
+    question: "SECTION 1 : 5. In the past two weeks, how often have you felt bad about yourself or like you're a failure?",
+    a: "NEVER",
+      b: "SOMETIMES",
+      c: "OFTEN",
+      d: " ALWAYS",
+      s1: "a",
+      s2: "b",
+      s3: "c",
+      s4: "d",
+    type: "deppression",
+  },
+  {
+    question: "SECTION 1 : 6. In the past two weeks, how often have you had trouble concentrating on things, such as reading or watching TV? ",
+    a: "NEVER",
+      b: "SOMETIMES",
+      c: "OFTEN",
+      d: " ALWAYS",
+      s1: "a",
+      s2: "b",
+      s3: "c",
+      s4: "d",
+    type: "deppression",
+  },
+  {
+    question: "SECTION 1 : 7. In the past two weeks, how often have you had thoughts that you would be better off dead, or thoughts of hurting yourself in some way? ",
+    a: "NEVER",
+      b: "SOMETIMES",
+      c: "OFTEN",
+      d: " ALWAYS",
+      s1: "a",
+      s2: "b",
+      s3: "c",
+      s4: "d",
+    type: "deppression",
+  },
+];
+const Anxitydata = [
+  {
+    question:
+      "SECTION 2 : 1.How often do you worry excessively about everyday activities such as work or school?",
+      a: "NEVER",
+      b: "SOMETIMES",
+      c: "OFTEN",
+      d: " ALWAYS",
+      s1: "a",
+      s2: "b",
+      s3: "c",
+      s4: "d",
+    type: "Anxiety",
+  },
+  {
+    question:
+      "SECTION 2 : 2. How often do you experience feelings of restlessness or being on edge?",
+      a: "NEVER",
+      b: "SOMETIMES",
+      c: "OFTEN",
+      d: " ALWAYS",
+      s1: "a",
+      s2: "b",
+      s3: "c",
+      s4: "d",
+    type: "Anxiety",
+  },
+  {
+    question:
+      "SECTION 2 : 3.How often do you have difficulty falling or staying asleep because of worrying thoughts?",
+      a: "NEVER",
+      b: "SOMETIMES",
+      c: "OFTEN",
+      d: " ALWAYS",
+      s1: "a",
+      s2: "b",
+      s3: "c",
+      s4: "d",
+    type: "Anxiety",
+  },
+  {
+    question: "SECTION 2 : 4. How often do you feel a sense of impending doom or danger?",
+    a: "NEVER",
+      b: "SOMETIMES",
+      c: "OFTEN",
+      d: " ALWAYS",
+      s1: "a",
+      s2: "b",
+      s3: "c",
+      s4: "d",
+    type: "Anxiety",
+  },
+  {
+    question: "SECTION 2 : 5.How often do you avoid social situations or events because of anxiety? ",
+    a: "NEVER",
+      b: "SOMETIMES",
+      c: "OFTEN",
+      d: " ALWAYS",
+      s1: "a",
+      s2: "b",
+      s3: "c",
+      s4: "d",
+    type: "Anxiety",
+  },
+  {
+    question: "SECTION 2 : 6.How often do you experience physical symptoms such as sweating, shaking, or heart palpitations when anxious?",
+    a: "NEVER",
+      b: "SOMETIMES",
+      c: "OFTEN",
+      d: " ALWAYS",
+      s1: "a",
+      s2: "b",
+      s3: "c",
+      s4: "d",
+    type: "Anxiety",
+  },
+  {
+    question: "SECTION 2 : 7. How often do you find yourself engaging in repetitive behaviors or rituals as a way of coping with anxiety?",
+    a: "NEVER",
+      b: "SOMETIMES",
+      c: "OFTEN",
+      d: " ALWAYS",
+      s1: "a",
+      s2: "b",
+      s3: "c",
+      s4: "d",
+    type: "Anxiety",
+  },
+];
+const stressdata = [
+  {
+    question:
+      "SECTION 3 : 1.How often do you feel overwhelmed by your responsibilities?",
+      a: "NEVER",
+      b: "SOMETIMES",
+      c: "OFTEN",
+      d: " ALWAYS",
+      s1: "a",
+      s2: "b",
+      s3: "c",
+      s4: "d",
+    type: "Stress",
+  },
+  {
+    question:
+      "SECTION 3 : 2.How often do you feel tense or anxious during the day? ",
+    a: "Certainly Yes",
+    a: "NEVER",
+      b: "SOMETIMES",
+      c: "OFTEN",
+      d: " ALWAYS",
+      s1: "a",
+      s2: "b",
+      s3: "c",
+      s4: "d",
+    type: "Stress",
+  },
+  {
+    question:
+      "SECTION 3 : 3.How often do you struggle to fall or stay asleep because of stress?",
+      a: "NEVER",
+      b: "SOMETIMES",
+      c: "OFTEN",
+      d: " ALWAYS",
+      s1: "a",
+      s2: "b",
+      s3: "c",
+      s4: "d",
+    type: "Stress",
+  },
+  {
+    question: "SECTION 3 : 4. How often do you feel physically exhausted due to stress?",
+    a: "NEVER",
+    b: "SOMETIMES",
+    c: "OFTEN",
+    d: " ALWAYS",
+    s1: "a",
+    s2: "b",
+    s3: "c",
+    s4: "d",
+    type: "Stress",
+  },
+  {
+    question: "SECTION 3 : 5. How often do you feel like you don't have enough time to get everything done?",
+    a: "NEVER",
+    b: "SOMETIMES",
+    c: "OFTEN",
+    d: " ALWAYS",
+    s1: "a",
+    s2: "b",
+    s3: "c",
+    s4: "d",
+    type: "Stress",
+  },
+  {
+    question: "SECTION 3 : 6.How often do you feel irritable or short-tempered because of stress?",
+    a: "NEVER",
+    b: "SOMETIMES",
+    c: "OFTEN",
+    d: " ALWAYS",
+    s1: "a",
+    s2: "b",
+    s3: "c",
+    s4: "d",
+    type: "Stress",
+  },
+  {
+    question: "SECTION 3 : 7. How often do you feel like you're not able to handle the demands placed on you?",
+    a: "NEVER",
+    b: "SOMETIMES",
+    c: "OFTEN",
+    d: " ALWAYS",
+    s1: "a",
+    s2: "b",
+    s3: "c",
+    s4: "d",
+    type: "Stress",
+  },
+];
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+
+const submitBtn = document.getElementById("submit");
+
+let currentQuiz = 0;
+let DeppQuiz = 0;
+let AQuiz = 0;
+let SQuiz = 0;
+let Dscore = 0;
+let Ascore = 0;
+let Sscore = 0;
+let Dres = " ";
+let Ares = " ";
+let Sres = " ";
+loadDeppQuiz();
+
+function loadDeppQuiz() {
+  deselectAnswers();
+  const currentQuizData = Depressiondata[DeppQuiz];
+
+  questionEl.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
+}
+
+function deselectAnswers() {
+  answerEls.forEach((answerEl) => (answerEl.checked = false));
+}
+
+function getSelected() {
+  let answer = "";
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
+  return answer;
+}
+submitBtn.addEventListener("click", () => {
+  const answer = getSelected();
+  if (answer) {
+    if (answer === Depressiondata[DeppQuiz].s1) {
+      Dscore = Dscore + 0;
+    } else if (answer === Depressiondata[DeppQuiz].s2) {
+      Dscore = Dscore + 1;
+    } else if (answer === Depressiondata[DeppQuiz].s3) {
+      Dscore = Dscore + 2;
+    } else if (answer === Depressiondata[DeppQuiz].s4) {
+      Dscore = Dscore + 3;
+    }
+    DeppQuiz++;
+    if (DeppQuiz < Depressiondata.length) {
+      loadDeppQuiz();
+      console.log("Depp quiz");
+      console.log(DeppQuiz);
+    } else {
+      loadAQuiz();
+      console.log("next quiz");
+    }
+  }
+});
+
+function loadAQuiz() {
+  deselectAnswers();
+  const currentQuizData = Anxitydata[AQuiz];
+
+  questionEl.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
+}
+
+function deselectAnswers() {
+  answerEls.forEach((answerEl) => (answerEl.checked = false));
+}
+
+function getSelected() {
+  let answer = "";
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
+  return answer;
+}
+submitBtn.addEventListener("click", () => {
+  const answer = getSelected();
+  if (answer) {
+    if (answer === Anxitydata[AQuiz].s1) {
+      Ascore = Ascore + 0;
+    } else if (answer === Anxitydata[AQuiz].s2) {
+      Ascore = Ascore + 1;
+    } else if (answer === Anxitydata[AQuiz].s3) {
+      Ascore = Ascore + 2;
+    } else if (answer === Anxitydata[AQuiz].s4) {
+      Ascore = Ascore + 3;
+    }
+    AQuiz++;
+    if (AQuiz < Anxitydata.length) {
+      loadAQuiz();
+      console.log("Anxity quiz");
+      console.log(AQuiz);
+    } else {
+      loadSQuiz();
+    }
+  }
+});
+function loadSQuiz() {
+  deselectAnswers();
+  const currentQuizData = stressdata[SQuiz];
+
+  questionEl.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
+}
+
+function deselectAnswers() {
+  answerEls.forEach((answerEl) => (answerEl.checked = false));
+}
+function gotohome(){
+  window.location.href="LOGIN.html";
+}
+function getSelected() {
+  let answer = "";
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
+  return answer;
+}
+submitBtn.addEventListener("click", () => {
+  const answer = getSelected();
+  if (answer) {
+    if (answer === stressdata[SQuiz].s1) {
+      Sscore = Sscore + 0;
+    } else if (answer === stressdata[SQuiz].s2) {
+      Sscore = Sscore + 1;
+    } else if (answer === stressdata[SQuiz].s3) {
+      Sscore = Sscore + 2;
+    } else if (answer === stressdata[SQuiz].s4) {
+      Sscore = Sscore + 3;
+    }
+    SQuiz++;
+    if (SQuiz < stressdata.length) {
+      loadSQuiz();
+    } 
+    else {
+      
+      if (Dscore < 5) {
+        Dres = "No depression";
+      } else if (Dscore >= 5 && Dscore < 10) {
+        Dres = "Mild depression";
+      } else {
+        Dres = "High depression";
+      }
+
+      if (Ascore < 5) {
+        Ares = "No anxiety";
+      } else if (Ascore >= 5 && Ascore < 10) {
+        Ares = "Mild anxiety";
+      } else {
+        Ares = "High anxiety";
+      }
+
+      if (Sscore < 5) {
+        Sres = "No stress";
+      } else if (Sscore >= 5 && Sscore < 10) {
+        Sres = "Mild stress";
+      } else {
+        Sres = "High stress";
+      }
+
+            quiz.innerHTML = `<center>
+            <h1>OUTPUT!!!!!!</h1>
+            <h2> FINAL RESULT!!!!!!! </h2>
+            <h3>Your Depression Level   ${Dscore} :  ${Dres}</h3>
+            <h3>Your Anxiety Level   ${Ascore} :  ${Ares} </h3>
+            <h3>Your Stress Level   ${Sscore} :   ${Sres}</h3>
+            </center>
+            <button onclick="gotohome()">Reload</button>`;
+  }
+  }
+});
